3d063d15a3a62be1d2d3ec3940dbdafac07a7703
Ítalo Diego <136415343+iDiegoPaes@users.noreply.github.com>
2024-11-17 19:10:06 -0300

    Add files via upload

---
+++ assets/style/Hotel Beija-Flor - Quartos/assets/scripts/responsive.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById('menu');
+
+menu.addEventListener('click', () => {
+    const navbar = document.getElementById('main-aside');
+    if (navbar.style.display === "block" || navbar.style.display === "") {
+        collapseNavBar();
+    } else {
+        showNavBar();
+    }
+});
+
+const collapseNavBar = () => {
+    const navbar = document.getElementById('main-aside');
+    const top = document.getElementById('top');
+    navbar.style.display = "none";
+}
+
+const showNavBar = () => {
+    const navbar = document.getElementById('main-aside');
+    navbar.style.display = "block";
+}
